7791403854cf249a5cb64e8e412936a7fb183584
jxia <jxia1111@gmail.com>
2015-10-10 23:32:43 -0400

    almost at mvp

--- visualization.js
+++ visualization.js
@@ -17,8 +17,8 @@ var svg = d3.select("body").append("svg")
 var link = svg.selectAll(".link"),
     node = svg.selectAll(".node");
 
-graphs = stringJSON.split("&$^^#%$%$#&"); //"{"a" : ["c","d","e"], "b":["a","g","h"]};
-graph = JSON.parse(graphs);
+//graphs = stringJSON.split("&$^^#%$%$#&");
+graph = /*JSON.parse(graphs);*/ {"a" : ["c","d","e"], "b":["a","g","h"]};
   var nodes = [];
   var edges = [];
   var skip = false;
