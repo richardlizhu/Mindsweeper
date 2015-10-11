749b1bceefa8db6009c35d7ed5c740d0c4d7d6bc
jxia <jxia1111@gmail.com>
2015-10-10 22:50:04 -0400

    communication between chrome extension and webpage

---
+++ datagatherer.js
@@ -0,0 +1,15 @@
+var stringJSON;
+
+function getData() {
+// The ID of the extension we want to talk to.
+var editorExtensionId = "abcdefghijklmnoabcdefhijklmnoabc";
+
+// Make a simple request:
+chrome.runtime.sendMessage(editorExtensionId, {openUrlInEditor: url},
+  function(response) {
+    if (!response.success)
+      handleError(url);
+
+    stringJSON = response;
+  });
+}
