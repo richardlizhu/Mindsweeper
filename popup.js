ac91453bec64f907739230355a150310c5148be0
Anthony Kuntz <akuntz@cmu.edu>
2015-10-11 11:10:27 -0400

    sdfugn

---
+++ popup.js
@@ -0,0 +1,25 @@
+
+runningList = [["Apples",20],["Transistors",70]]
+neighbors = ["Bananas","Quantum Computing"]
+
+
+var sortByPref = function(runningList){
+  runningList.sort( function(x, y){ return y[1] - x[1] } )
+}
+
+var change = function(){
+  //sortByPref(msg.runningList);
+  //newTitle = relatedButton(neighbors, runningLIst, function(result){return result;});
+  var n = Math.floor(Math.random()*neighbors.length);
+
+  document.getElementById("textarea").value = neighbors[n];
+}
+
+window.onload = function() {
+  startup();
+}
+
+var startup = function(){
+  var btn = document.getElementById("myBtn");
+  btn.addEventListener("click",function(){change();});
+}
